a8364de5da7c9c6b69727fda5f49b1036ab81299
bahek_mel <bahek_mel@mail.ru>
2022-03-06 11:21:48 +0300

    T12, U9 3/3 Успех

--- search-server/process_queries.cpp
+++ search-server/process_queries.cpp
@@ -2,10 +2,10 @@
 
 std::vector<std::vector<Document>> ProcessQueries(
 		const SearchServer& search_server,
-		const std::vector<std::string> queries) {
+		const std::vector<std::string>& queries) {
 	std::vector<std::vector<Document>> result(queries.size());
 	std::transform(
-			//std::execution::par,
+			std::execution::par,
 			queries.cbegin(), queries.cend(),
 			result.begin(),
 			[&search_server](const std::string& query) {
@@ -16,21 +16,18 @@ std::vector<std::vector<Document>> ProcessQueries(
 }
 
 std::vector<Document> ProcessQueriesJoined(
-		const SearchServer& search_server,
-		const std::vector<std::string> queries) {
-	std::vector<Document> result;
+	const SearchServer& search_server,
+	const std::vector<std::string>& queries) {
 	std::vector<std::vector<Document>> documents = ProcessQueries(search_server, queries);
+
+	std::vector<Document> result;
+	result.reserve(documents.size());
 	for (const std::vector<Document>& document: documents) {
-		std::transform(
-				//std::execution::par,
-				document.cbegin(), document.cend(),
-				//result.begin(),
-				std::back_inserter(result),
-				[](const Document& doc) {
-					return doc;
-				}
-		);
+		std::transform(document.cbegin(), document.cend(),
+						std::back_inserter(result),
+						[](const Document& doc) {
+						return doc;}
+					);
 	}
 	return result;
 }
-
--- search-server/read_input_functions.h
+++ search-server/read_input_functions.h
@@ -1,21 +1,3 @@
-//#pragma once
-//
-//#include "document.h"
-//
-//#include <iostream>
-//#include <string>
-//#include <vector>
-//
-//using namespace std::string_literals;
-//
-//std::ostream& operator<<(std::ostream& out, const Document& document);
-//
-//void PrintDocument(const Document& document);
-//
-//void PrintMatchDocumentResult(int document_id, const std::vector<std::string>& words,
-//	DocumentStatus status);
-
-
 #pragma once
 
 #include "document.h"
